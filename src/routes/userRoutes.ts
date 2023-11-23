9b51202a70edf93f41be9f3c0221846966bf43c6
Milos Naskovic <m.naskovic@shindiristudio.com>
2023-11-23 14:46:31 +0100

    feat: created user register route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from "express";
-// import User from "../models/userModel";
+import User from "../models/userModel";
 
 const router = express.Router();
 
-router.post("/register", (req: Request, res: Response) => {
-  res.send(req.body);
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email });
+    await User.register(user, password);
+
+    res.json({ success: true, message: "Registration successful" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Registration failed" });
+  }
 });
 
 export default router;
